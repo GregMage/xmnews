e5b7a690e98ea3851e66c669a8b260b8551a7e0a
Grégory Mage <gregory.mage@gmail.com>
2020-02-03 21:11:41 +0100

    oubli traduction

--- language/french/blocks.php
+++ language/french/blocks.php
@@ -19,4 +19,5 @@
 // index.php
 define('_MB_XMNEWS_ALLCATEGORY', 'Toutes les catégories');
 define('_MB_XMNEWS_CATEGORY', 'Afficher les catégories:');
+define('_MB_XMNEWS_NBNEWS', 'Nombre d\'article à afficher');
 define('_MB_XMNEWS_FULL', 'Voir l\'article complet');
