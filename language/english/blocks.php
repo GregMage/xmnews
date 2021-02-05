8e92c84400c752c1c592c770328d60c881018a1a
alain01 <alainternet01@free.fr>
2021-02-05 15:22:09 +0100

    MAJ bloc carrousel
    - Ajout image pour article sans image pour le carrousel
    - Ajout du chemin via smarty xoops_url
    - Mise en avant du titre et du résumé avec un fond standard (bg-info)
    - Adaptation du code pour avoir une hauteur max fixe en cas d'image ne cadrant pas dans le carrousel
    
    Il restera sans doute à ajouter une option pour permettre au wembaster de proposer une hauteur max, sans quoi la hauteur max est définie ici en dur à 360 pixels.

--- language/english/blocks.php
+++ language/english/blocks.php
@@ -18,18 +18,18 @@
  */
 // index.php
 define('_MB_XMNEWS_ABSTRACT', 'Number of words to display for the abstract of the news');
-define('_MB_XMNEWS_ABSTRACT_DESC', '(Put "all" to have no limit and "0" to not display the description)');
+define('_MB_XMNEWS_ABSTRACT_DESC', '("all": no limit, "0": not display the abstracts)');
 define('_MB_XMNEWS_ALLCATEGORY', 'All Categories');
-define('_MB_XMNEWS_CATEGORY', 'Display category:');
+define('_MB_XMNEWS_CATEGORY', 'Display category');
 define('_MB_XMNEWS_NBNEWS', 'Number of news to display');
-define('_MB_XMNEWS_NEWS', 'Display news:');
+define('_MB_XMNEWS_NEWS', 'Display news');
 define('_MB_XMNEWS_FULL', 'See the full news');
 define('_MB_XMNEWS_TYPE', 'Block type');
 define('_MB_XMNEWS_TYPE_DATE', 'Recent News');
 define('_MB_XMNEWS_TYPE_HITS', 'Top News (hits)');
 define('_MB_XMNEWS_TYPE_RATING', 'Top Rated News');
 define('_MB_XMNEWS_TYPE_RANDOM', 'Random News');
-define('_MB_XMNEWS_SIZE', 'Image size in pixel');
+define('_MB_XMNEWS_SIZE', 'Image width in pixel');
 define('_MB_XMNEWS_LOGO', 'Show logo');
 define('_MB_XMNEWS_NEWS_CAROUSEL', 'All news (category)');
 define('_MB_XMNEWS_NEWS_CAROUSEL_DESC', 'If you choose articles, the category filter is not used. If you want to use the category filter, you should only use the first option. ');
