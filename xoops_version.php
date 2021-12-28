21f59d9f63c68a5ece6ff21226e1cdadc3a64f08
GregMage <gregory.mage@gmail.com>
2021-12-28 16:14:23 +0100

    Version 1.6 RC1

--- xoops_version.php
+++ xoops_version.php
@@ -409,8 +409,8 @@ $modversion['notification']['event'][] = [
 ];
 
 // About stuff
-$modversion['module_status'] = 'Alpha';
-$modversion['release_date']  = '2021/03/20';
+$modversion['module_status'] = 'RC1';
+$modversion['release_date']  = '2021/12/28';
 
 $modversion['developer_lead']      = 'Mage';
 $modversion['module_website_url']  = 'www.monxoops.fr/';
